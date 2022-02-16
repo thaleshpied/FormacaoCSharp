735b99d2ce8a3ee4b7a2d81b04ca9079a765a19b
Thales <thaleshpied@gmail.com>
2022-02-15 21:45:52 -0300

    2-Criando variáveis

---
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto 2, criando variáveis");
+
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+            Console.WriteLine("Execução finalizada, tecle enter para sair...");
+
+            Console.ReadLine();
+        }
+    }
+}
