1f242d3702c0fae3c5d95290655dd244d79b418a
Thales <thaleshpied@gmail.com>
2022-02-15 21:30:08 -0300

    1 - Primeiro Projeto

---
+++ AprendendoCSharp/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+
+           Console.WriteLine("Hello, World!");
+           Console.WriteLine("Executado");
+            Console.ReadLine();
+            
+        }
+    }
+}
